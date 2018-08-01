337f2f6211f790f6fcfcc440a60c0558d0d0609a
Daniel Wang <daniel.wang.1999@hotmail.com>
2018-07-31 23:06:25 -0400

    frontend react started

---
+++ app-client/src/store/reducers/errors.js
@@ -0,0 +1,8 @@
+import {ADD_ERROR, REMOVE_ERROR} from "../actionTypes";
+
+export default (state = {message: null}, action) => {
+    switch(action.type) {
+        case ADD_ERROR:
+            return 
+    }
+}
