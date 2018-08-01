438d6bd4de5bf523b5f87634917656fa1cf244e7
Daniel Wang <daniel.wang.1999@hotmail.com>
2018-07-31 23:33:34 -0400

    redux

--- app-client/src/store/reducers/errors.js
+++ app-client/src/store/reducers/errors.js
@@ -3,6 +3,6 @@ import {ADD_ERROR, REMOVE_ERROR} from "../actionTypes";
 export default (state = {message: null}, action) => {
     switch(action.type) {
         case ADD_ERROR:
-            return 
+            return  
     }
 }
