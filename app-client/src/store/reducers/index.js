87e53cc0e382b9d15306535c852cc62a73a1e3e2
Daniel Wang <daniel.wang.1999@hotmail.com>
2018-08-02 22:08:37 -0400

    config store

--- app-client/src/store/reducers/index.js
+++ app-client/src/store/reducers/index.js
@@ -1,1 +1,10 @@
 import {combineReducers} from "redux"; 
+import currentUSer from "./currentUser";
+import errors from "./errors";
+
+const rootReducer = combineReducers({
+    currentUser,
+    errors
+})
+
+export default rootReducer;
