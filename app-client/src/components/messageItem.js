c24de3ea0881794ba3da9ad64da6d672ef2ef378
Daniel Wang <daniel.wang.1999@hotmail.com>
2018-08-08 00:19:10 -0400

    bug fixes

--- app-client/src/components/messageItem.js
+++ app-client/src/components/messageItem.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom"
 import DefaultProfileImg from "../images/default-profile-image.jpg"
 
 const MessageItem = ({date, profileImageUrl, text, username}) => {
+    return (
     <div>
         <img src={profileImageUrl || DefaultProfileImg} alt={username} height="100" width="100" className="timeline-image"/>
         <div className="message-area">
@@ -14,6 +15,7 @@ const MessageItem = ({date, profileImageUrl, text, username}) => {
             <p>{text}</p>
         </div>
     </div>
+    )
 }
 
 export default MessageItem;
