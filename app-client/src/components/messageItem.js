c8991b2dcecf311efa471080f7ba8a441385fdc6
Daniel Wang <daniel.wang.1999@hotmail.com>
2018-08-11 16:03:15 -0400

    delete button

--- app-client/src/components/messageItem.js
+++ app-client/src/components/messageItem.js
@@ -11,29 +11,32 @@ const MessageItem = ({
   removeMessage,
   isCorrectUser
 }) => (
-  <div>
-    <li className="list-group-item">
-      <img
-        src={profileImageUrl || DefaultProfileImg}
-        alt={username}
-        height="100"
-        width="100"
-        className="timeline-image"
-      />
-      <div className="message-area">
-        <Link to="/">@{username} &nbsp;</Link>
-        <span className="text-muted">
-          <Moment className="text-muted" format="Do MMM YYYY">
-            {date}
-          </Moment>
-        </span>
-        <p>{text}</p>
-        <a className ="btn btn-danger" onClick={removeMessage}>
-          Delete
-        </a>
-      </div>
-    </li>
-  </div>
-);
+    <div>
+      <li className="list-group-item">
+        <img
+          src={profileImageUrl || DefaultProfileImg}
+          alt={username}
+          height="100"
+          width="100"
+          className="timeline-image"
+        />
+        <div className="message-area">
+          <Link to="/">@{username} &nbsp;</Link>
+          <span className="text-muted">
+            <Moment className="text-muted" format="Do MMM YYYY">
+              {date}
+            </Moment>
+          </span>
+          <p>{text}</p>
+          {isCorrectUser && (
+            <a className="btn btn-danger" onClick={removeMessage}>
+              Delete
+            </a>
+          )}
+
+        </div>
+      </li>
+    </div>
+  );
 
 export default MessageItem;
